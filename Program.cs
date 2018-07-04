162ac46bc54ef6633cbfaa57362c318c190dd453
ChaBrizu <carlos.chabrizu@gmail.com>
2018-07-04 11:56:21 -0500

    Algoritmo de la mochila con reparación aleatoria terminado
    Corregidos todos los errores aparentes.
    Arroja el resultado más óptimo.
    Programado del carajo, tho.

--- Program.cs
+++ Program.cs
@@ -10,8 +10,8 @@ namespace AlgoritmoMochila
     class Program
     {   
         //variables del problema de la mochila
-        static int[] peso;
-        static int[] ganancia;
+        static int[] peso=new int[objetos];
+        static int[] ganancia = new int[objetos];
         static int capacidad_mochila=0;
         static int objetos=0;
         static int mochilas=0;
@@ -19,29 +19,46 @@ namespace AlgoritmoMochila
         //Variables del algoritmo genético
         static double pm=0;
         static double pc=0;
-        
-        public String[] binPadres = new String[' '];
-        public String[] binHijos = new String[' '];
 
-        int[] xPeso = new int[' '];
-        int[] fxGanancia = new int[' '];
-        double suma = 0.0;
-        double sumfnorm = 0.0;
+        Double[] acumulado = new Double[mochilas];
 
-        double[] mejoresResultadosGen = new double[' '];
-        double[] mejoresResultadosGlobal = new double[' '];
+        public String[] binPadres = new String[mochilas];
+        public String[] binHijos = new String[mochilas];
+
+        int[] xPeso = new int[mochilas];
+        int[] fxGanancia = new int[mochilas];
+
+        //Resultados
+        int[] mejoresPesosGen = new int[generaciones];
+        int[] mejoresGananciasGen = new int[generaciones];
+        String[] mejoresBin = new String[generaciones];
 
         static int generaciones = 0;
 
         static void Main(string[] args)
-        {
-            Program obj = new Program();
+        {            
             load_data();            
 
             foreach (int value in peso)
             {
                 objetos++;
             }
+
+            Console.Write("Pesos de los objetos: ");
+
+            for (int i = 0; i < objetos; i++)
+            {
+                Console.Write(peso[i] + ", ");
+            }
+
+            Console.Write("\nGanancias de los objetos: ");
+
+            for (int i = 0; i < objetos; i++)
+            {
+                Console.Write(ganancia[i] + ", ");
+            }
+
+            Console.WriteLine("\nCapacidad de la mochila: " + capacidad_mochila);
 
             Console.Write("Escribe la cantidad de individuos (mochilas) a evaluar: ");
             while (mochilas <= 0 || mochilas % 2 != 0)
@@ -74,48 +91,89 @@ namespace AlgoritmoMochila
                 }
             }
 
+
             Console.Write("Escribe el número de generaciones a evaluar: ");
             while (generaciones <= 0)
             {
                 generaciones = Convert.ToInt32(Console.ReadLine());
             }
 
-            Console.Write("\nPesos de los objetos: ");
+            Program obj = new Program();
 
-            for (int i=0; i<objetos; i++)
+            obj.generar_individuos(obj);
+            obj.calculoMochila(obj);
+
+            for (int i = 0; i < mochilas; i++)
             {
-                Console.Write(peso[i] +", ");
+                if (obj.xPeso[i] > capacidad_mochila)
+                {
+                    String auxBin = obj.binPadres[i];
+                    obj.reparacion(auxBin, i, obj);
+                }
             }
 
-            Console.Write("\nGanancias de los objetos: ");
-
-            for (int i = 0; i < objetos; i++)
+            for (int auxGen = 0; auxGen < generaciones; auxGen++)
             {
-                Console.Write(ganancia[i] + ", ");
-            }
+                obj.algoritmo_Genetico(obj);
+                obj.calculoMochila(obj);
 
-            Console.WriteLine("Capacidad de la mochila: " + capacidad_mochila);
-            
-            obj.generar_individuos();
-            obj.calculoMochila();
-
-            for(int auxGen = 0; auxGen < generaciones; auxGen++)
-            {
-                for(int i = 0; i < mochilas; i++)
+                for (int i = 0; i < mochilas; i++)
                 {
                     if (obj.xPeso[i] > capacidad_mochila)
                     {
-                        Console.WriteLine("Mochila " + (i + 1) + " sobrepasa el peso máximo");
                         String auxBin = obj.binPadres[i];
-                        obj.binPadres[i] = obj.reparacion(auxBin, i);
-                    }                    
-                }                
+                        obj.reparacion(auxBin, i, obj);
+                    }
+                }
+
+                int auxGanancia = 0;
+                int mejorGan = 0;
+                int mejorPeso = 0;
+                String mejorBin = "";
+
+                for (int i = 0; i < mochilas; i++)
+                {
+                    for (int j = mochilas - 1; j >= i; j--)
+                    {
+                        auxGanancia = obj.fxGanancia[i];
+                        if (auxGanancia > mejorGan)
+                        {
+                            mejorGan = auxGanancia;
+                            mejorPeso = obj.xPeso[i];
+                            mejorBin = obj.binPadres[i];
+                        }
+                    }
+                }          
+        
+                obj.mejoresPesosGen[auxGen] = mejorPeso;
+                obj.mejoresGananciasGen[auxGen] = mejorGan;
+                obj.mejoresBin[auxGen] = mejorBin;
             }
 
-            Console.ReadKey();
+            int auxGananciaGen = 0;
+            int mejorGanGeneracion = 0;
+            int mejorPesoGen = 0;
+            String mejorBinGen = "";
+
+            for (int i = 0; i < generaciones; i++)
+            {
+                for (int j = generaciones - 1; j >= i; j--)
+                {
+                    auxGananciaGen = obj.mejoresGananciasGen[i];
+                    if (auxGananciaGen > mejorGanGeneracion)
+                    {
+                        mejorGanGeneracion = auxGananciaGen;
+                        mejorPesoGen = obj.mejoresPesosGen[i];
+                        mejorBinGen = obj.mejoresBin[i];
+                    }
+                }
+            }
+
+            Console.WriteLine("Carga óptima con un peso de " + mejorPesoGen.ToString() + ", con una ganancia de " + mejorGanGeneracion.ToString()+ ", \nsiendo su cadena binaria " + mejorBinGen);
+            Console.ReadKey();            
         }
 
-        public void generar_individuos()
+        public void generar_individuos(Program obj)
         {
             Random r = new Random();
             for(int i=0; i < mochilas; i++)
@@ -126,75 +184,170 @@ namespace AlgoritmoMochila
                     int rand = r.Next(0, 2);
                     binario = binario + Convert.ToString(rand);
                 }
-                binPadres[i] = binario;
-                Console.WriteLine("Individuo " + (i + 1) + ": " + binPadres[i]);
+                obj.binPadres[i] = binario;
             }            
         }
 
-        public void calculoMochila()
+        public void calculoMochila(Program obj)
         {
             char[] individuo = new char[' '];
             for (int i = 0; i < mochilas; i++)
             {
-                individuo = binPadres[i].ToCharArray();
+                obj.xPeso[i] = 0;
+                obj.fxGanancia[i] = 0;
+
+                individuo = obj.binPadres[i].ToCharArray();
                 for (int j = 0; j < individuo.Length; j++)
                 {
                     if (individuo[j] == '1')
                     {
-                        xPeso[i] = xPeso[i] + peso[j];
-                        //fxGanancia[i] = fxGanancia[i] + ganancia[j];
-                    }
+                        obj.xPeso[i] = obj.xPeso[i] + peso[j];
+                        obj.fxGanancia[i] = obj.fxGanancia[i] + ganancia[j];
+                    }                    
                 }
-                Console.WriteLine("Peso de la mochila " + (i + 1) + ": " + xPeso[i]);
             }
         }
 
-        public String reparacion(String auxBin, int i)
+        public void reparacion(String auxBin, int i, Program obj)
         {
-            Console.WriteLine("ENTRA A REPARACIÓN");
-            String x = auxBin;
-            Boolean mochila_llena = false;
+            Boolean mochila_llena = true;
+            char[] x = auxBin.ToArray();
 
-            Random rand = new Random();            
+            Random rand = new Random();
 
             int auxR;
-            int auxPeso = xPeso[i];
+            int auxPeso = obj.xPeso[i];
+            int auxGanancia = obj.fxGanancia[i];
 
-            if (auxPeso > capacidad_mochila)
+            while(mochila_llena)
             {
-                mochila_llena = true;
-                Console.WriteLine("ENTRA A IF EN"+ mochila_llena);
-            }
+                auxR = rand.Next(0, x.Length);
 
-            char[] reparador = x.ToCharArray();
-
-            do
-            {
-                Console.WriteLine("ENTRA A WHILE EN" + mochila_llena);
-
-                auxR = rand.Next(auxBin.Length);
-                if (reparador[auxR] == '1')
+                if (x[auxR] == '1')
                 {
-                    Console.WriteLine("ENTRA A PRIMER IF DEL WHILE EN" + reparador[auxR]);
-                    reparador[auxR] = '0';
-                    Console.WriteLine("Y SALE DE PRIMER IF DEL WHILE EN" + reparador[auxR]);
+                    x[auxR] = '0';
+                    auxPeso = auxPeso - peso[auxR];
+                    auxGanancia = auxGanancia - ganancia[auxR];
                 }
-                for (int j = 0; j < reparador.Length; j++)
-                {
-                    if (reparador[j] == '1')
-                    {
-                        auxPeso = auxPeso + peso[j];
-                    }
-                }
+
                 if (auxPeso <= capacidad_mochila)
                 {
                     mochila_llena = false;
-                    Console.WriteLine("NUEVO individuo " + (i + 1) + ": " + x);
+                    obj.xPeso[i] = auxPeso;
+                    obj.fxGanancia[i] = auxGanancia;
                 }
-                x = reparador.ToString();
-            } while (mochila_llena);
-            xPeso[i] = auxPeso;
-            return x;
+            }
+
+            String xmod = new string(x);
+            obj.binPadres[i] = xmod;
+        }
+
+        public void algoritmo_Genetico(Program obj)
+        {
+            double suma = 0.0;
+            double sumfnorm = 0.0;
+
+            Double[] fnorm = new Double[mochilas];
+            Double[] acumulado = new Double[mochilas];
+            String[] newpadres = new String[mochilas];
+
+            for (int i = 0; i < mochilas; i++)
+            {
+                suma += obj.fxGanancia[i];
+            }
+
+            for (int i = 0; i < mochilas; i++)
+            {
+                fnorm[i] = obj.fxGanancia[i] / suma;
+                sumfnorm += fnorm[i];
+                acumulado[i] = sumfnorm;
+            }
+
+            Random aleatorio = new Random();
+
+            Double[] vector = new Double[mochilas];
+
+            for (int i = 0; i < mochilas; i++)
+            {
+                vector[i] = aleatorio.NextDouble();
+            }
+
+            for (int i = 0; i < mochilas; i++)
+            {
+                for (int j = 0; j < mochilas; j++)
+                {
+                    if (acumulado[j] > vector[i])
+                    {
+                        newpadres[i] = obj.binPadres[j];
+                        break;
+                    }
+                }
+            }
+
+            int corte1, corte2;
+
+            Random random = new Random();
+            Random cruce = new Random();
+
+            //Cruce
+            for (int i = 0; i < mochilas; i = i + 2)
+            {
+                if (cruce.NextDouble() <= pc)
+                {
+                    do
+                    {
+                        corte1 = random.Next(1, objetos);
+                        corte2 = random.Next(1, objetos);
+                    } while (corte1 == corte2);
+                    
+                    if (corte1 < corte2)
+                    {
+                        obj.binHijos[i] = newpadres[i].Substring(0, corte1) + newpadres[i + 1].Substring(corte1, corte2 - corte1) + newpadres[i].Substring(corte2, objetos - corte2);
+                        obj.binHijos[i + 1] = newpadres[i + 1].Substring(0, corte1) + newpadres[i].Substring(corte1, corte2 - corte1) + newpadres[i + 1].Substring(corte2, objetos - corte2);
+                    }
+
+                    else
+                    {
+                        obj.binHijos[i] = newpadres[i + 1].Substring(0, corte2) + newpadres[i].Substring(corte2, corte1 - corte2) + newpadres[i + 1].Substring(corte1, objetos - corte1);
+                        obj.binHijos[i + 1] = newpadres[i].Substring(0, corte2) + newpadres[i + 1].Substring(corte2, corte1 - corte2) + newpadres[i].Substring(corte1, objetos - corte1);
+                    }                    
+                }
+                else
+                {
+                    obj.binHijos[i] = newpadres[i];
+                    obj.binHijos[i + 1] = newpadres[i + 1];
+                }
+            }
+            //Fin cruce
+
+            //Mutación
+            char[] mutacion = new char[' '];
+            for (int i = 0; i < mochilas; i++)
+            {
+                mutacion = obj.binHijos[i].ToCharArray();//Separa los strings en caracteres
+                for (int j = 0; j < mutacion.Length; j++)
+                {
+                    if (random.NextDouble() < pm)
+                    {
+                        if (mutacion[j] == '1')
+                        {
+                            mutacion[j] = '0';
+                        }
+                        if (mutacion[j] == '0')
+                        {
+                            mutacion[j] = '1';
+                        }
+                    }
+                }
+                obj.binHijos[i] = new string(mutacion);                
+            }
+            //Fin mutación
+
+            //Asigna el valor de los hijos a padres
+            for(int i=0; i < mochilas; i++)
+            {
+                obj.binPadres[i] = obj.binHijos[i];
+            }
         }
 
         public static void load_data()
